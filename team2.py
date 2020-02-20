d38467e5424b0e4f2ab8f0977fc9df9a0e16b8b9
chloe-chin <chloemchin@gmail.com>
2020-02-20 06:02:18 +0000

    Team 2: We fixed some bugs in the final draft of our three strategies.

--- team2.py
+++ team2.py
@@ -28,10 +28,10 @@ def move(my_history, their_history, my_score, their_score):
     
     return 'c'
 
-strategy1_name = 'collude then betray twice'
-strategy1_description = 'In this strategy, the player will start off by colluding and then betray for the next two moves. After the two betrays, they will collude again'
+strategy_name = 'collude then betray twice'
+strategy_description = 'In this strategy, the player will start off by colluding and then betray for the next two moves. After the two betrays, they will collude again'
 
-def move1(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score):
   '''Make my move based on the history with this player.
     
     history: a string with one letter (c or b) per round that has been played with this opponent.
@@ -41,15 +41,15 @@ def move1(my_history, their_history, my_score, their_score):
     
     Returns 'c' or 'b' for collude or betray.
     '''
-  if len(my_history) == 0 or len(my_history) % 3 == 0
+  if len(my_history) == 0 or len(my_history) % 3 == 0:
     return 'c'
   else:
     return 'b'
 
-strategy2_name = 'return opposite'
-strategy2_description = 'If the opponent betrays, then collude next round. If the opponent colludes, then betray the next round. first round is collude.'
+strategy_name = 'return opposite'
+strategy_description = 'If the opponent betrays, then collude next round. If the opponent colludes, then betray the next round. first round is collude.'
 
-def move2(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score):
   '''Make my move based on the history with this player.
     
     history: a string with one letter (c or b) per round that has been played with this opponent.
@@ -59,17 +59,17 @@ def move2(my_history, their_history, my_score, their_score):
     
     Returns 'c' or 'b' for collude or betray.
     '''
-    if len(my_history) == 0:
-      return 'c'
-    elif their_history[-1] == 'b':
-      return 'c'
-    else:
-      return 'b'
+  if len(my_history) == 0:
+    return 'c'
+  elif their_history[-1] == 'b':
+    return 'c'
+  else:
+    return 'b'
 
-strategy3_name = 'If collude, then collude. If betray, then betray twice'
-strategy3_description = 'One where if the opponent colludes, then this player will collude for the next turn. If the opponent betrays, this player will betray for the next 2 turns. This dependence on the opponent’s turn will be reset at the collude or the 2nd betrayal of this player. Betrayal is first turn'
+strategy_name = 'If collude, then collude. If betray, then betray twice'
+strategy_description = 'One where if the opponent colludes, then this player will collude for the next turn. If the opponent betrays, this player will betray for the next 2 turns. This dependence on the opponent’s turn will be reset at the collude or the 2nd betrayal of this player. Betrayal is first turn'
 
-def move3(my_history, their_history, my_score, their_score):
+def move(my_history, their_history, my_score, their_score):
   '''Make my move based on the history with this player.
     
     history: a string with one letter (c or b) per round that has been played with this opponent.
